6d6d051d3e3df6bcf640e8f5394114c15511f28d
VasysMS <vasiavasia10000@gmail.com>
2024-02-10 01:54:10 +0300

    Bug fixed...

--- streetcat_viewer.py
+++ streetcat_viewer.py
@@ -8,7 +8,7 @@ sleeps = ["sl", "http://streetcatpull.hellobike.com/live/4489918405732275808_0.m
 snack = ["sn", "http://streetcatpull.hellobike.com/live/4291094747934800009_0.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_1.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_2.m3u8"]
 shock = ["sh", "http://streetcatpull.hellobike.com/live/4584985755015398729_0.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_1.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_2.m3u8"]
 
-cams = [fresh_1]
+cams = [fresh_1, fresh_2, snack, shock]
 cam_proc = None
 
 def play(command = "ffplay", parameters = "", cam_name = "", cam_number = 1):
@@ -21,7 +21,9 @@ def play(command = "ffplay", parameters = "", cam_name = "", cam_number = 1):
 
     global cam_proc
     if cam_proc: 
-        cam_proc.stdin.write('q'.encode('utf-8'))
-        cam_proc.stdin.flush()
+        try:
+            cam_proc.stdin.write('q'.encode('utf-8'))
+            cam_proc.stdin.flush()
+        except: None
     cam_proc = subprocess.Popen((command + " " + cam_url + " " + parameters), shell=True, stdin=subprocess.PIPE)
     return cam_proc
