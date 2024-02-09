2dd52e059da86046c193672c818d49735f513def
VasysMS <vasiavasia10000@gmail.com>
2024-02-09 21:08:57 +0300

    use of os.system() has been replaced by subprocess.Popen()

--- console.py
+++ console.py
@@ -1,4 +1,18 @@
 import streetcat_viewer
 
 while(True):
-    streetcat_viewer.select_and_play()
+    print("Cats Cams:")
+    print(" Mr Fresh: f_1 or f_2")
+    print(" Miss Sleeps: sl")
+    print(" Mr Snack: sn")
+    print(" Mr Shock: sh")
+    
+    print("Enter camera name: ", end="")
+    cam_name = input()
+    cam_url = ""
+
+    for cam in streetcat_viewer.cams:
+        if cam_name == cam[0]:
+            print("Cam number: ", end="")
+            cam_url = cam[int(input())]
+    streetcat_viewer.play(command = "ffplay", parameters = "", cam_url = cam_url)
--- streetcat_viewer.py
+++ streetcat_viewer.py
@@ -1,25 +1,13 @@
-import os
+import subprocess
 
-def select_and_play(command = "ffplay", parameters = ""):
-    print("Cats Cams:")
-    print(" Mr Fresh: f_1 or f_2")
-    print(" Miss Sleeps: sl")
-    print(" Mr Snack: sn")
-    print(" Mr Shock: sh")
+fresh_1 = ["f_1", "http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8", "http://streetcatpull.hellobike.com/live/4258783365322591678_1.m3u8", "http://streetcatpull.hellobike.com/live/4258783365322591678_2.m3u8"]
+fresh_2 = ["f_2", "http://streetcatpull.hellobike.com/live/4412424173050749216_0.m3u8", "http://streetcatpull.hellobike.com/live/4412424173050749216_1.m3u8", "http://streetcatpull.hellobike.com/live/4412424173050749216_2.m3u8"]
+sleeps = ["sl", "http://streetcatpull.hellobike.com/live/4489918405732275808_0.m3u8", "http://streetcatpull.hellobike.com/live/4489918405732275808_1.m3u8", "http://streetcatpull.hellobike.com/live/4489918405732275808_2.m3u8"]
+snack = ["sn", "http://streetcatpull.hellobike.com/live/4291094747934800009_0.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_1.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_2.m3u8"]
+shock = ["sh", "http://streetcatpull.hellobike.com/live/4584985755015398729_0.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_1.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_2.m3u8"]
 
-    print("Enter camera name: ", end="")
-    fresh_1 = ["f_1", "http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8", "http://streetcatpull.hellobike.com/live/4258783365322591678_1.m3u8", "http://streetcatpull.hellobike.com/live/4258783365322591678_2.m3u8"]
-    fresh_2 = ["f_2", "http://streetcatpull.hellobike.com/live/4412424173050749216_0.m3u8", "http://streetcatpull.hellobike.com/live/4412424173050749216_1.m3u8", "http://streetcatpull.hellobike.com/live/4412424173050749216_2.m3u8"]
-    sleeps = ["sl", "http://streetcatpull.hellobike.com/live/4489918405732275808_0.m3u8", "http://streetcatpull.hellobike.com/live/4489918405732275808_1.m3u8", "http://streetcatpull.hellobike.com/live/4489918405732275808_2.m3u8"]
-    snack = ["sn", "http://streetcatpull.hellobike.com/live/4291094747934800009_0.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_1.m3u8", "http://streetcatpull.hellobike.com/live/4291094747934800009_2.m3u8"]
-    shock = ["sh", "http://streetcatpull.hellobike.com/live/4584985755015398729_0.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_1.m3u8", "http://streetcatpull.hellobike.com/live/4584985755015398729_2.m3u8"]
+cams = [fresh_1, fresh_2, sleeps, snack, shock]
 
-    cams = [fresh_1, fresh_2, sleeps, snack, shock]
-    cam_url = ""
-    t = input()
-    for cam in cams:
-        if t == cam[0]:
-            print("Cam number: ", end="")
-            cam_url = cam[int(input())]
-
-    os.system(command + " " + cam_url + " " + parameters)
+def play(command = "ffplay", parameters = "", cam_url = fresh_1[1]):
+    cam_proc = subprocess.Popen(command + " " + cam_url + " " + parameters)
+    cam_proc.wait()
